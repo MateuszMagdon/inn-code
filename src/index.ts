07f14673b1cae1ec61cdab7c89e94c7c930574ee
Matt Mcdon <mateuszm@gmail.com>
2020-06-05 22:33:23 +0200

    basic pricelist defined + simple select + deselect implemented

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,30 @@ export type ServiceType = "Photography" | "VideoRecording" | "BlurayPackage" | "
 export const updateSelectedServices = (
     previouslySelectedServices: ServiceType[],
     action: { type: "Select" | "Deselect"; service: ServiceType }
-) => [];
+) => {
+    switch (action.type) {
+        case "Select":
+            return previouslySelectedServices.push(action.service);
+        case "Deselect":
+            let index = previouslySelectedServices.indexOf(action.service);
+            return index > -1 ? previouslySelectedServices.splice(index, 1) : previouslySelectedServices;
+    }
+};
 
 export const calculatePrice = (selectedServices: ServiceType[], selectedYear: ServiceYear) => ({ basePrice: 0, finalPrice: 0 });
+
+interface Service {
+    type: ServiceType,
+    prices: Price[]
+}
+
+interface Price {
+    year: number,
+    price: number
+}
+
+const store: Service[] =
+[
+    {type: "Photography", prices: [{year: 2020, price: 1700},{year: 2021, price: 1800},{year:2022, price:1900}]},
+    {type: "VideoRecording", prices: [{year: 2020, price: 1700},{year: 2021, price: 1800},{year:2022, price:1900}]},
+]
