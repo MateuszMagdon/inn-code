7bc9e1e465d1f0995fc5463c27b66689dc3b66e6
Matt Mcdon <mateuszm@gmail.com>
2020-06-05 23:36:57 +0200

    Select and deselect working

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,54 @@ export const updateSelectedServices = (
 ) => {
     switch (action.type) {
         case "Select":
-            return [...previouslySelectedServices, action.service];
+            return selectAction(previouslySelectedServices, action.service);
         case "Deselect":
-            return previouslySelectedServices.filter(s => s !== action.service);
+            return deselectAction(previouslySelectedServices, action.service);
         default:
             return previouslySelectedServices;
     }
 };
 
+function selectAction(state: ServiceType[], service: ServiceType): ServiceType[] {
+    const alreadyExists = state.includes(service);
+    const serviceObject = mainServices.find(s => s.type === service);
+    const canBeAdded = !serviceObject.applicableIf
+        || state.filter(val => serviceObject.applicableIf.includes(val)).length > 0;
+
+    return !alreadyExists && canBeAdded ? [...state, service] : state;
+}
+
+function deselectAction(state: ServiceType[], service: ServiceType): ServiceType[] {
+    const newState = state.filter(s => s !== service);
+    const newStateObjects = mainServices.filter(s => newState.includes(s.type));
+    const toRemove = newStateObjects
+        .filter(
+            s => s.applicableIf && s.applicableIf.filter(val => newState.includes(val)).length === 0)
+        .map(s => s.type);
+
+    return newState.filter(s => !toRemove.includes(s))
+}
+
 export const calculatePrice = (selectedServices: ServiceType[], selectedYear: ServiceYear) => ({ basePrice: 0, finalPrice: 0 });
 
 interface Service {
     type: ServiceType,
-    prices: Price[]
+    price?: number,
+    priceList?: PriceInYear[],
+    applicableIf?: ServiceType[]
 }
 
-interface Price {
+interface PriceInYear {
     year: number,
     price: number
 }
 
-const store: Service[] =
+const mainServices: Service[] =
 [
-    {type: "Photography", prices: [{year: 2020, price: 1700},{year: 2021, price: 1800},{year:2022, price:1900}]},
-    {type: "VideoRecording", prices: [{year: 2020, price: 1700},{year: 2021, price: 1800},{year:2022, price:1900}]},
+    {type: "Photography", priceList: [{year: 2020, price: 1700},{year: 2021, price: 1800},{year:2022, price:1900}]},
+    {type: "VideoRecording", priceList: [{year: 2020, price: 1700},{year: 2021, price: 1800},{year:2022, price:1900}]},
+    //photo+video package: 2020: 2200, 2021: 2300, 2022: 2500
+    {type: "WeddingSession", price: 600}, // 300 if video or photo; 0 if photo in 2022
+    {type: "BlurayPackage", price: 300, applicableIf: ["VideoRecording"]},
+    {type: "TwoDayEvent", price: 400, applicableIf: ["Photography", "VideoRecording"]}
 ]
